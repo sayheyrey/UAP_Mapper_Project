67a54e523b0b29b77760d9a5e8b7bc019f3631d7
REY <reygarciajr88@gmail.com>
2025-04-03 20:28:17 -0600

    Delete config.js
    grrr

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-const firebaseConfig = {
-    apiKey: "AIzaSyBy05x8Df5zOjdjJ7wJs7T7UEK-S1BYDOk",
-    authDomain: "interactive-uap-mapping.firebaseapp.com",
-    projectId: "interactive-uap-mapping",
-    storageBucket: "interactive-uap-mapping.firebasestorage.app",
-    messagingSenderId: "94894681827",
-    appId: "1:94894681827:web:36ebe619918485e5799fd6",
-  };
